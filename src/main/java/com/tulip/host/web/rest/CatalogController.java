2acac8354d3b7cf86fd5d7d4650cd9b5c2d3914d
Sanket Saha <sanketsaha@gmail.com>
2023-02-11 20:31:45 +0530

    Update CatalogController.java

--- src/main/java/com/tulip/host/web/rest/CatalogController.java
+++ src/main/java/com/tulip/host/web/rest/CatalogController.java
@@ -2,8 +2,11 @@ package com.tulip.host.web.rest;
 
 import com.tulip.host.data.CatalogDTO;
 import com.tulip.host.data.FeesCatalogDTO;
+import com.tulip.host.data.InventoryItemDTO;
 import com.tulip.host.service.CatalogService;
 import com.tulip.host.web.rest.vm.CatalogVM;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +20,9 @@ public class CatalogController {
 
     @GetMapping("/fees/{classID}")
     public List<FeesCatalogDTO> feesCatalog(@PathVariable Long classID) {
-        return catalogService.fetchFeesCatalog(classID);
+        List<FeesCatalogDTO> feesCatalogDTOS = catalogService.fetchFeesCatalog(classID);
+        Collections.sort(feesCatalogDTOS, Comparator.comparing(FeesCatalogDTO::getName));
+        return feesCatalogDTOS;
     }
 
     @GetMapping("/product/{classID}")
