51ae67f9426db2063447f3ad55703b21e3747c54
Sanket Saha <sanketsaha@gmail.com>
2023-07-04 10:14:24 +0530

    Update V3__CreateTable.sql

--- src/main/resources/db/migration/V3__CreateTable.sql
+++ src/main/resources/db/migration/V3__CreateTable.sql
@@ -1,8 +1,39 @@
---ALTER TABLE upload
---ADD transaction_id bigint,
---ADD FOREIGN KEY (transaction_id) REFERENCES transactions (transaction_id);
---
---
---ALTER TABLE expense
---ADD unit_price double NOT NULL;
---
+ALTER TABLE IF NOT EXISTS upload
+ADD transaction_id bigint,
+ADD FOREIGN KEY (transaction_id) REFERENCES transactions (transaction_id);
+
+ALTER TABLE transactions
+ADD COLUMN due_opted bit(1)  DEFAULT 0;
+
+ALTER TABLE expense
+ADD unit_price double NOT NULL;
+
+CREATE TABLE IF NOT EXISTS dues (
+  id BIGINT AUTO_INCREMENT NOT NULL,
+   transaction_id BIGINT NULL,
+   due_date datetime NOT NULL,
+   amount DOUBLE NOT NULL,
+   approved_by VARCHAR(255) NOT NULL,
+   status VARCHAR(255) NOT NULL,
+   penalty DOUBLE NOT NULL,
+   created_by VARCHAR(50) NOT NULL,
+   created_date datetime NULL,
+   last_modified_by VARCHAR(50) NULL,
+   last_modified_date datetime NULL,
+   PRIMARY KEY (id),
+   FOREIGN KEY (transaction_id) REFERENCES transactions (transaction_id)
+);
+
+CREATE TABLE IF NOT EXISTS dues_payment (
+  id BIGINT AUTO_INCREMENT NOT NULL,
+   amount DOUBLE NOT NULL,
+   penalty DOUBLE NOT NULL,
+   payment_mode VARCHAR(50) NOT NULL,
+   due_id BIGINT NOT NULL,
+   created_by VARCHAR(50) NOT NULL,
+   created_date datetime NULL,
+   last_modified_by VARCHAR(50) NULL,
+   last_modified_date datetime NULL,
+  	PRIMARY KEY (id),
+  	FOREIGN KEY (due_id) REFERENCES dues (id)
+);
