ef9ca667debd823219bc02abddb0e41188afe91e
Sanket Saha <sanketsaha@gmail.com>
2024-01-20 11:08:38 +0530

    CHANGES TO UPDATE Q CLASS (#63)
    * Update pom.xml
    
    * Update pom.xml
    
    * Revert "Update pom.xml"
    
    This reverts commit e7fe89e46ab3245ee8de74344e1d421092f5e1f4.
    
    * Update ObjectStorageService.java
    
    * Update pom.xml
    
    * fixed null on updated docs & added name to appointment letter
    
    * make the expense fix and upload functionality
    
    * Changes to update to LocalTime added calender & data upload
    
    * Update bootstrap-prod.yml
    
    * Update bootstrap-prod.yml
    
    * Changes to update fileds and add new tables
    
    * CHANGES TO UPDATE Q CLASS

--- src/main/java/com/tulip/host/repository/AcademicCalendarRepository.java
+++ src/main/java/com/tulip/host/repository/AcademicCalendarRepository.java
@@ -3,4 +3,4 @@ package com.tulip.host.repository;
 import com.tulip.host.domain.AcademicCalendar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HolidayRepository extends JpaRepository<AcademicCalendar, Long> {}
+public interface AcademicCalendarRepository extends JpaRepository<AcademicCalendar, Long> {}
