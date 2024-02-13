b5680df7a2ec1e41c0e130a6717e846bc7d6bbbe
Sanket Saha <sanketsaha@gmail.com>
2024-02-13 13:25:33 +0530

    Changes to sort class based on order (#66)
    * Update pom.xml
    
    * Update pom.xml
    
    * Revert "Update pom.xml"
    
    This reverts commit e7fe89e46ab3245ee8de74344e1d421092f5e1f4.
    
    * Update ObjectStorageService.java
    
    * Update pom.xml
    
    * fixed null on updated docs & added name to appointment letter
    
    * make the expense fix and upload functionality
    
    * Changes to update to LocalTime added calender & data upload
    
    * Update bootstrap-prod.yml
    
    * Update bootstrap-prod.yml
    
    * Changes to update fileds and add new tables
    
    * CHANGES TO UPDATE Q CLASS
    
    * Update DataLoadController.java
    
    * added pdf and upload qualification
    
    * Changes to sort class based on order

---
+++ src/main/java/com/tulip/host/enums/ClassTypeEnum.java
@@ -0,0 +1,28 @@
+package com.tulip.host.enums;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public enum ClassTypeEnum {
+    NURSERY(0),
+    LKG(1),
+    UKG(2),
+    I(3),
+    II(4),
+    III(5),
+    IV(6),
+    V(7),
+    VI(8),
+    VII(9),
+    VIII(10),
+    IX(11),
+    X(12);
+
+    private final int order;
+
+    ClassTypeEnum(int order) {
+        this.order = order;
+    }
+}
