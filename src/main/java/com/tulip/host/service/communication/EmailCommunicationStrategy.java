c3de02d262c9976c641d6ce2fbc0df13c6a11772
Sanket Saha <sanketsaha@gmail.com>
2026-01-11 22:28:56 +0530

    Fix auth for staff

--- src/main/java/com/tulip/host/service/communication/EmailCommunicationStrategy.java
+++ src/main/java/com/tulip/host/service/communication/EmailCommunicationStrategy.java
@@ -1,6 +1,7 @@
 package com.tulip.host.service.communication;
 
 import com.tulip.host.config.ApplicationProperties;
+import com.tulip.host.domain.OutboundCommunication;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
@@ -28,7 +29,7 @@ public class EmailCommunicationStrategy implements CommunicationStrategy {
     }
 
     @Override
-    public void send(CommunicationRequest request) {
+    public void send(CommunicationRequest request, OutboundCommunication outboundCommunication) {
         if (isDevProfile(env.getDefaultProfiles())) {
             request.setRecipient(new String[]{properties.getTwilioConfig().getDefaultEmail()});
             request.setCc(new String[]{});
